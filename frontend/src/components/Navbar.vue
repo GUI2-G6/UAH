b1e3a84799c1021c4927d048b846e87330a15eb7
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-03-07 23:59:31 -0500

    Split some of the stylings into their own files

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="nav">
+    <router-link to="/" class="nav-link">Home</router-link>
+    <router-link to="/status" class="nav-link">Status</router-link>
+    </nav>
+</template>
+
+<!--Exports the Navbar so other fieles can see and use it-->
+<script>
+    export default{
+        name: "Navbar"
+    }
+</script>
+
+<style src="./css/Navbar.css"></style>
