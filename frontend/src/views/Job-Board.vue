dc615b5461beae3e9db659044b7e4f7580c29d68
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-03-21 22:54:00 -0400

    Added job board page

--- frontend/src/views/Job-Board.vue
+++ frontend/src/views/Job-Board.vue
@@ -1,9 +1,24 @@
 <template>
-    <p>Job Board</p>
+    <div class="page">
+        <div class="greeting">
+            <h1>Job Board</h1>
+            <p>Browse and apply to jobs in one click!</p> <!-- Add links to actual variables here! -->
+        </div>
+        <div class="dashboard">
+            <div class="card">Job 1</div>
+            <div class="card">Job 2</div>
+            <div class="card">Job 3</div>
+            <div class="card">Job 4</div>
+            <div class="card">Job 5</div>
+            <div class="card">Job 6</div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default{
-        name: "Job-Board"
+        name: "Home"
     }
 </script>
+
+<style src="./css/Job-board.css"></style>
