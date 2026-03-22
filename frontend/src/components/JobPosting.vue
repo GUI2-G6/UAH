349196ef44ec6df16084282a6f6cf2535d336270
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-03-22 15:03:50 -0400

    Created mock job-postings in Job Board

---
+++ frontend/src/components/JobPosting.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card job-card">
+        <h3>{{ job.title }}</h3>
+        <p>{{ job.company }}</p>
+        <p>{{ job.location }}</p>
+        <button @click="apply">Apply</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "JobPosting",
+    props: {
+        job: Object
+    },
+    methods: {
+        apply() {
+            console.log("Applying to", this.job.title);
+        }
+    }
+}
+</script>
--- frontend/src/views/Job-Board.vue
+++ frontend/src/views/Job-Board.vue
@@ -2,23 +2,40 @@
     <div class="page">
         <div class="greeting">
             <h1>Job Board</h1>
-            <p>Browse and apply to jobs in one click!</p> <!-- Add links to actual variables here! -->
+            <p>Browse and apply to jobs in one click!</p>
         </div>
         <div class="dashboard">
-            <div class="card">Job 1</div>
-            <div class="card">Job 2</div>
-            <div class="card">Job 3</div>
-            <div class="card">Job 4</div>
-            <div class="card">Job 5</div>
-            <div class="card">Job 6</div>
+            <JobPosting 
+                v-for="job in jobs" 
+                :key="job.id" 
+                :job="job"
+            />
         </div>
     </div>
 </template>
 
+
 <script>
-    export default{
-        name: "Home"
-    }
+import JobPosting from "../components/JobPosting.vue";
+
+export default {
+  name: "JobBoard",
+  components: { JobPosting },
+
+  data() {
+    return {
+      jobs: []
+    };
+  },
+
+  mounted() {
+    this.jobs = [
+      { id: 1, title: "Software Engineer", company: "Google", location: "Remote" },
+      { id: 2, title: "Frontend Dev", company: "Amazon", location: "NYC" },
+      { id: 3, title: "Backend Dev", company: "Meta", location: "SF" }
+    ];
+  }
+}
 </script>
 
 <style src="./css/Job-board.css"></style>
