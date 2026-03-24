17d7a13045385a59e62c2f07e04ea91f97018bae
ramenoodles <ramksenthil@gmail.com>
2026-03-24 14:28:24 -0400

    Created signup page
    Looks very similar to login page

---
+++ frontend/src/views/Signup.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="page">
+        <h1>Login</h1>
+            <p>Track every application in one place</p>
+            <input type="email" v-model="email" placeholder="Enter Your Email to Begin account" />
+            <button @click="singup">Submit</button>
+            <div class="signup-row">
+            <span>Already have an account? </span>
+            <a @click.prevent="login" href="#">Return to Login Page</a>
+            </div>
+        </div>
+</template>
+
+
+<script>
+    export default{
+        name: "Signup"
+    }
+</script>
+
+<style src="./css/Login.css"></style>
