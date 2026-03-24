739e75bd399256a1bebb0249927c0d18a6893fee
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-03-23 22:06:30 -0400

    Started work on Settings page and card components

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+        <div v-if="$slots.header" class="header">
+            <slot name="header" />
+        </div>
+        <div class="body">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Card"
+    }
+</script>
+
+<style src="./css/Card.css"></style>
--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -4,27 +4,54 @@
             <h1>Settings</h1>
         </div>
         <div class="dashboard">
-            <div class="card">
-                <h1>Applied: 0</h1>
-            </div>
-            <div class="card">
-                <h1>Interviews: 0</h1>
-            </div>
-            <div class="card">
-                <h1>Offers: 0</h1>
-            </div>
-            <div class="card">
-                <h1>Rejected: 0</h1>
-            </div>
+            <Card>
+                <template #header>
+                    <h3>Profile Settings</h3>
+                </template>
+                <p>Full Name:</p>
+                <input type="text"> <!--v-model="name" (potential for updating variables)-->
+                <p>Job Title:</p>
+                <input type="text">
+            </Card>
+            <Card>
+                <template #header>
+                    <h3>Notifications</h3>
+                </template>
+                <h5>Email Notifications</h5>
+            </Card>
+            <Card>
+                <template #header>
+                    <h3>Account & Security</h3>
+                </template>
+                <p>Full Name:</p>
+                <input type="text"> <!--v-model="name" (potential for updating variables)-->
+                <p>Job Title:</p>
+                <input type="text">
+            </Card>
+            <Card>
+                <template #header>
+                    <h3>Preferences</h3>
+                </template>
+                <p>Full Name:</p>
+                <input type="text"> <!--v-model="name" (potential for updating variables)-->
+                <p>Job Title:</p>
+                <input type="text">
+            </Card>
+
         </div>
     </div>
 
 </template>
 
 <script>
-    export default{
-        name: "Settings"
-    }
+import Card from "../components/Card.vue";
+
+export default {
+  name: "Settings",
+  components: {
+    Card
+  }
+}
 </script>
 
 <style src="./css/Settings.css"></style>
