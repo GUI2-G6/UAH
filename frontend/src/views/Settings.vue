489357b81ffd9640c15c0d840285e9b5ff59e20a
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-03-23 22:09:30 -0400

    Added more to Settings.vue template

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -17,25 +17,23 @@
                 <template #header>
                     <h3>Notifications</h3>
                 </template>
-                <h5>Email Notifications</h5>
+                <h4>Email Notifications</h4>
+                <h4>Reminder Notifications</h4>
+                <h4>Application Status Updates</h4>
             </Card>
             <Card>
                 <template #header>
                     <h3>Account & Security</h3>
                 </template>
-                <p>Full Name:</p>
-                <input type="text"> <!--v-model="name" (potential for updating variables)-->
-                <p>Job Title:</p>
-                <input type="text">
+                <h4>Change Email</h4>
+                <h4>Change Password</h4>
             </Card>
             <Card>
                 <template #header>
                     <h3>Preferences</h3>
                 </template>
-                <p>Full Name:</p>
-                <input type="text"> <!--v-model="name" (potential for updating variables)-->
-                <p>Job Title:</p>
-                <input type="text">
+                <h4>Language</h4>
+                <h4>Timezone</h4>
             </Card>
 
         </div>
