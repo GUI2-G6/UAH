9b551f402f3322772429b84e06b4f0de6049fbe1
Nj3125 <nicholas_johnson5@student.uml.edu>
2026-04-07 00:02:31 -0400

    Update Toast.vue

--- frontend/src/components/Toast.vue
+++ frontend/src/components/Toast.vue
@@ -8,15 +8,15 @@
 
 <script>
 export default {
-  props: {
-    message: { type: String, required: true },
-    type: { type: String, default: 'success' } // 'success' | 'error'
-  },
-  data() {
-    return { visible: true };
-  },
-  mounted() {
-    setTimeout(() => this.visible = false, 3000); // auto-hide after 3s
-  }
+    props: {
+        message: { type: String, required: true },
+        type: { type: String, default: 'success' } // 'success' | 'error'
+    },
+    data() {
+        return { visible: true };
+    },
+    mounted() {
+        setTimeout(() => this.visible = false, 3000); // auto-hide after 3s
+    }
 };
 </script>
